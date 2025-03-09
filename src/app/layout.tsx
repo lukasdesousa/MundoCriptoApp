11b28a7a545e1ce0809b2c398f72110ee44716bb
= <lukassousa.ls13@gmail.com>
2025-03-08 21:44:51 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ const inter = Inter({
   subsets: ['latin'],
 })
 
-import { Analytics } from '@vercel/analytics/react';
 import { Provider } from "react-redux";
 import { GlobalStyle } from "@/styles/GlobalStyle";
 import { store } from "@/store/store";
@@ -16,7 +15,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR" className={inter.className}>
       <body>
-          <Analytics />
         <Provider store={store}>
           <GlobalStyle />
           {children}
